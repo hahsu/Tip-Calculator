2f2dc05851f6abf856736090605ab36268c41a20
Hannah Hsu <hahsu@fb.com>
2018-06-26 17:40:47 -0700

    Added a few new functionalities and completed README

--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -11,9 +11,16 @@
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipTitleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalTitleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
+@property (weak, nonatomic) IBOutlet UITextField *numPeopleField;
+@property (weak, nonatomic) IBOutlet UILabel *perPersonLabel;
+@property (weak, nonatomic) IBOutlet UILabel *numPeopleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *perPersonTitleLabel;
+
 
 @end
 
@@ -21,6 +28,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.tipTitleLabel setHidden:YES];
+    [self.tipLabel setHidden:YES];
+    [self.totalTitleLabel setHidden:YES];
+    [self.totalLabel setHidden:YES];
+    [self.tipControl setHidden:YES];
+    [self.numPeopleField setHidden:YES];
+    [self.perPersonLabel setHidden:YES];
+    [self.numPeopleLabel setHidden:YES];
+    [self.perPersonTitleLabel setHidden:YES];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -31,7 +49,6 @@
 }
 
 - (IBAction)onTap:(id)sender {
-    NSLog(@"Hello");
     [self.view endEditing:YES];
 }
 - (IBAction)onEdit:(id)sender {
@@ -41,32 +58,47 @@
     
     double tip = tipPercentage * bill;
     double total = bill + tip;
+    double tipPerPerson = tip / [self.numPeopleField.text doubleValue];
     
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+    if([self.numPeopleField.text doubleValue] == 0 || [self.billField.text doubleValue] == 0){
+        self.perPersonLabel.text = @"$0.00";
+    }
+    else{
+        self.perPersonLabel.text = [NSString stringWithFormat:@"$%.2f", tipPerPerson];
+    }
+    
 }
+- (IBAction)numPeopleEdit:(id)sender {
+    double bill = [self.billField.text doubleValue];
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    double tip = tipPercentage * bill;
+    double tipPerPerson = tip / [self.numPeopleField.text doubleValue];
+    if([self.numPeopleField.text doubleValue] == 0 || [self.billField.text doubleValue] == 0){
+        self.perPersonLabel.text = @"$0.00";
+    }
+    else{
+    self.perPersonLabel.text = [NSString stringWithFormat:@"$%.2f", tipPerPerson];
+    }
+}
+
 
 - (IBAction)onEditingBegin:(id)sender {
-    [UIView animateWithDuration:0.2 animations:^{
-        self.billField.frame = CGRectMake(self.billField.frame.origin.x, self.billField.frame.origin.y + 30, self.billField.frame.size.width, self.billField.frame.size.height);
-    }];
-    
     [UIView animateWithDuration:1 animations:^{
-        self.tipLabel.alpha = 0;
+        
+        [self.tipTitleLabel setHidden:NO];
+        [self.tipLabel setHidden:NO];
+        [self.totalTitleLabel setHidden:NO];
+        [self.totalLabel setHidden:NO];
+        [self.tipControl setHidden:NO];
+        [self.numPeopleField setHidden:NO];
+        [self.perPersonLabel setHidden:NO];
+        [self.numPeopleLabel setHidden:NO];
+        [self.perPersonTitleLabel setHidden:NO];
     }];
 }
 
-- (IBAction)onEditingEnd:(id)sender {
-    CGRect newFrame = self.billField.frame;
-    newFrame.origin.y -= 30;
-    
-    [UIView animateWithDuration:0.2 animations:^{
-        self.billField.frame = newFrame;
-    }];
-    
-    [UIView animateWithDuration:1 animations:^{
-        self.tipLabel.alpha = 1;
-    }];
-}
 
 @end
